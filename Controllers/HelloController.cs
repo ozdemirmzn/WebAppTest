5998b21749d8309b347f85f7a67f4b98f7beee5a
Ramazan Ozdemir <ozdemirmzn@gmail.com>
2020-12-01 18:52:14 -0600

    test

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppTest.Controllers
+{
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        [Route("/helloworld/")]
+        public IActionResult Index()
+        {
+            string html = "<form method='post' action='/helloworld/'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+            return Content(html, "text/html");
+        }
+
+        /*[HttpGet]
+        [Route("/helloworld/welcome/{name?}")]*/
+        [HttpPost]
+        [Route("/helloworld/")]
+        public IActionResult Welcome(string name = "World!")
+        {
+            return Content("<h1>Welcome to my app, " + name + " </h1>", "text/html");
+        }
+    }
+}
