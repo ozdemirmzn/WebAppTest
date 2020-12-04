5e92d14067d6194287f081cd9bfbaf97518722b8
Ramazan Ozdemir <ozdemirmzn@gmail.com>
2020-12-03 18:57:02 -0600

    test

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -6,27 +6,27 @@ using System.Threading.Tasks;
 
 namespace WebAppTest.Controllers
 {
-    [Route("/helloworld/")]
+    
     public class HelloController : Controller
     {
         [HttpGet]
         
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/helloworld/'>" +
-                "<input type='text' name='name' />" +
-                "<input type='submit' value='Greet Me!' />" +
-                "</form>";
-            return Content(html, "text/html");
+            return View();
         }
 
         /*[HttpGet]
         [Route("/helloworld/welcome/{name?}")]*/
-        [HttpGet("welcome/{name?}")]
+        //[HttpGet("welcome/{name?}")]
         [HttpPost]
+        [Route("/hello/")]
         public IActionResult Welcome(string name = "World!")
         {
-            return Content("<h1>Welcome to my app, " + name + " </h1>", "text/html");
+            ViewBag.person = name;
+            return View();
+            
+            // return Content("<h1>Welcome to my app, " + name + " </h1>", "text/html");
         }
     }
 }
